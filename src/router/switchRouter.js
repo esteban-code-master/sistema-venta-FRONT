f3e7989386d070e5d9f1e024f40b5b6f8627fe5d
Esteban Guzman <67021790+esteban-code-master@users.noreply.github.com>
2021-08-02 14:53:26 -0500

    apis

--- src/router/switchRouter.js
+++ src/router/switchRouter.js
@@ -10,7 +10,22 @@ const SwitchRouter = () => {
         <Switch>
             <Route
                 path = "/app/dashboard"
-            />
+            >
+                <div style = {{padding:'30px'}}>
+                    <button style = {{padding:'30px',marginRight: '20px'}} onClick = {async ()=>{
+                            await fetch('http://localhost:8001/api/pdpf?gmail=canepaalexis@gmail.com')                             
+                            alert('se envio pdf')
+                    }}>
+                        ENVIAR PDF
+                    </button>
+                    <button style = {{padding:'30px',marginRight: '20px'}} onClick = {async()=>{
+                        await fetch('http://localhost:8001/api/pdpf?excel=canepaalexis@gmail.com')                        
+                        alert('se envio excel')
+                    }}>
+                        ENVIAR EXCEL
+                    </button>
+                </div>
+            </Route>
             <Route path ="/app/post">    
                 <div id="root-template"></div>              
                <Tabla2 />                              
